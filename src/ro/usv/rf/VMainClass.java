bcbdc781dcdbccc0fe9263cb663dd4cd4dad3ba9
vost4o <vasyl.ostafi@gmail.com>
2020-04-13 10:13:24 +0300

    Initialize Java project

---
+++ src/ro/usv/rf/VMainClass.java
@@ -0,0 +1,16 @@
+package ro.usv.rf;
+
+/**
+ * Main class of the application
+ * @author vasyl
+ *
+ */
+public class VMainClass {
+	/**
+	 * Main method
+	 * @param args - application parameters
+	 */
+	public static void main(String... args) {
+		
+	}
+}
